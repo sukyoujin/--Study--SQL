72cdd1aaaa1902823b5cce565c451635889da0a9
Yujin Suk <52108994+sukyoujin@users.noreply.github.com>
2019-08-10 20:54:34 +0900

    Update 석유진조영욱.sql

--- 석유진조영욱.sql
+++ 석유진조영욱.sql
@@ -1,13 +1,14 @@
 
 결과***
-석유진이 오늘부터 엔코아 과정이 끝날 때까지 매주 알코올100g(약 맥주 5잔)을 마신다면 당신이 죽는 날(다른 변수 제외, 평균수명 기준, 연도만 따진 날짜)
-select add_months(add_months(sysdate, round(85.74 - 27, 0)*12), -((52-to_number(to_char(to_date('2019-07-19','yyyy-mm-dd'),'WW')) + to_number(to_char(to_date('2020.01.16','yyyy-mm-dd'), 'WW')))*6)) from dual;
+
+Q. 석유진이 오늘부터 엔코아 과정이 끝날 때까지 매주 알코올100g(약 맥주 5잔)을 마신다면 당신이 죽는 날(다른 변수 제외, 평균수명 기준, 연도만 따진 날짜)
+A. select add_months(add_months(sysdate, round(85.74 - 27, 0)*12), -((52-to_number(to_char(to_date('2019-07-19','yyyy-mm-dd'),'WW')) + to_number(to_char(to_date('2020.01.16','yyyy-mm-dd'), 'WW')))*6)) from dual;
 ADD_MONT
 --------
 65/07/19
 
-조영욱이 오늘부터 엔코아 과정이 끝날 때까지 매주 알코올100g(약 맥주 5잔)을 마신다면 당신이 죽는 날(다른 변수 제외, 평균수명 기준, 연도만 따진 날짜)
-select add_months(add_months(sysdate, round(80.50 - 26, 0)*12), -((52-to_number(to_char(to_date('2019-07-19','yyyy-mm-dd'),'WW')) + to_number(to_char(to_date('2020.01.16','yyyy-mm-dd'), 'WW')))*6)) from dual;
+Q. 조영욱이 오늘부터 엔코아 과정이 끝날 때까지 매주 알코올100g(약 맥주 5잔)을 마신다면 당신이 죽는 날(다른 변수 제외, 평균수명 기준, 연도만 따진 날짜)
+A. select add_months(add_months(sysdate, round(80.50 - 26, 0)*12), -((52-to_number(to_char(to_date('2019-07-19','yyyy-mm-dd'),'WW')) + to_number(to_char(to_date('2020.01.16','yyyy-mm-dd'), 'WW')))*6)) from dual;
 ADD_MONT
 --------
 61/07/19
@@ -34,4 +35,20 @@ select (52 - to_number(to_char(to_date('2019-07-19','yyyy-mm-dd'),'WW')) + to_nu
 
 
 
+in English----------------------------------------------------------------------
+				 
+Result***
+				 
+Q. If Yujin(woman) starts drinking 100 grams of alcohol every day, when will she die?(Excluding other variables, based on life expectancy, date based on year only)
+A. select add_months(add_months(sysdate, round(85.74 - 27, 0)*12), -((52-to_number(to_char(to_date('2019-07-19','yyyy-mm-dd'),'WW')) + to_number(to_char(to_date('2020.01.16','yyyy-mm-dd'), 'WW')))*6)) from dual;
+ADD_MONT
+--------
+65/07/19
 
+Q. If Youngwook(male) starts drinking 100 grams of alcohol every day, when will she die?(Excluding other variables, based on life expectancy, date based on year only)
+A. select add_months(add_months(sysdate, round(80.50 - 26, 0)*12), -((52-to_number(to_char(to_date('2019-07-19','yyyy-mm-dd'),'WW')) + to_number(to_char(to_date('2020.01.16','yyyy-mm-dd'), 'WW')))*6)) from dual;
+ADD_MONT
+--------
+61/07/19
+
+Process***
